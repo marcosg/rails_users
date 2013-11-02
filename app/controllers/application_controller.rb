3fcbc9ffffbcd9aa163f4a73fae33b5d13025c89
Marcos Galigarcia <marcosg101@gmail.com>
2013-11-02 17:37:34 -0400

    Select default language based on browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,24 @@ class ApplicationController < ActionController::Base
 	private
 
 	def set_locale
-	  I18n.locale = params[:locale] if params[:locale].present?
+	  #I18n.locale = params[:locale] if params[:locale].present?
 	  # current_user.locale
 	  # request.subdomain
 	  # request.env["HTTP_ACCEPT_LANGUAGE"]
 	  # request.remote_ip
+
+	  if params[:locale].present?
+	  	I18n.locale = params[:locale]
+	  else
+	  	I18n.locale = "#{I18n.default_locale}"
+		  language = request.env['HTTP_ACCEPT_LANGUAGE']    
+		  if language
+		    language = language.scan(/^[a-z]{2}/).first
+		    if language.match /^(#{I18n.available_locales.join("|")})$/
+		      I18n.locale = language
+		  	end
+		  end
+		end
 	end
 
 	def default_url_options(options = {})
