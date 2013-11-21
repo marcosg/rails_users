76bee83a940b34fc94d3e0b48f464a77d09ded6c
Marcos Galigarcia <marcosg101@gmail.com>
2013-11-21 05:26:49 -0500

    clean up testing for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,11 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(page_title) }
+  end
+
   describe "Language links" do
     before do
       visit help_path
@@ -39,30 +44,50 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('VetRx') }
-    it { should have_title(full_title('')) }
+    let(:heading)    { 'VetRx' }
+    let(:page_title) { 'VetRx' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About VetRx' }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('About VetRx')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title('About VetRx')
+    click_link "Help"
+    expect(page).to have_title('Help')
+    click_link "Contact"
+    expect(page).to have_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title('Sign up')
+    click_link "VetRx"
+    expect(page).to have_title('VetRx')
   end
 end
